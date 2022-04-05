c414f680fa0eaf8b76f1ce2d1ec7a24948782e54
olivia zhao <68890723+oliviaosi@users.noreply.github.com>
2022-04-05 19:50:53 +0800

    created a new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import{useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // State
+  const[message, setMessage] = useState(
+    "JavaScript is the best"
+    );
+  // create an array with two viarable in side. Usestate allows you to hold on to the memory, 
+  // setMessage allows to update
+
+  // Action
+  function handleClick(){
+    setMessage("Some new message saying sth smart");
+  }
+
+  // UI What appears on the screen, with Javascript on the top, inject the state
+  return(
+    <div>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Update the message</button>
     </div>
   );
 }
 
 export default App;
+
